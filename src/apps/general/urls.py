89b11164396e894f3f156a011f909125e22aa773
Oveys Safarnejad <safarnejad@outlook.com>
2023-05-19 18:21:06 +0200

    add address endpoints

---
+++ src/apps/general/urls.py
@@ -0,0 +1,9 @@
+from rest_framework import routers
+
+from apps.general.api import CityViewSet
+
+
+app_name = 'general'
+
+router = routers.DefaultRouter()
+router.register('cities', CityViewSet, 'Cities')
--- src/apps/user/urls.py
+++ src/apps/user/urls.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
-from apps.user.api import UserViewSet
+from apps.user.api import AddressViewSet, UserViewSet
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet, 'Users')
+router.register('addresses', AddressViewSet, 'Addresses')
