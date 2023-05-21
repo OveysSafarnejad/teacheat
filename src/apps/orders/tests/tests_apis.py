b3fa16bd84044476217916c682c5b1bb01e133e6
Oveys Safarnejad <safarnejad@outlook.com>
2023-05-21 12:09:01 +0200

    add test for user's orders list endpoint

--- src/apps/orders/tests/tests_apis.py
+++ src/apps/orders/tests/tests_apis.py
@@ -9,6 +9,7 @@ from model_bakery import baker
 from apps.core.tests import BaseAPITestCase
 from apps.tasties.models import Tasty
 from apps.user.models import Address, User
+from apps.orders.models import Order
 
 
 class OrdersTest(BaseAPITestCase):
@@ -17,6 +18,11 @@ class OrdersTest(BaseAPITestCase):
         self.tasty = baker.make(Tasty)
         self.address = baker.make(Address)
         self.user = baker.make(User)
+
+        address_data = {
+            'owner': self.user
+        }
+        self.client_user_address = baker.make(Address, **address_data)
         self.client.force_authenticate(user=self.user)
 
     def test_create_order_invalid_address_error_400(self):
@@ -47,20 +53,35 @@ class OrdersTest(BaseAPITestCase):
         response = json.loads(response.content)
         self.assertEqual(response['delivery'], ['You can choose your delivery date from the next day.'])
 
+    def test_create_order_successful_201(self):
 
-def test_create_order_successful_201(self):
-    address_data = {
-        'owner': self.user.id
-    }
-    address = baker.make(Address, **address_data)
-    order_data = {
+        order_data = {
+                'delivery': timezone.now() + timedelta(days=2),
+                'quantity': 2,
+                'tasty': self.tasty.id,
+                'address': self.client_user_address.id
+        }
+
+        url = reverse('orders:order-list')
+        response = self.client.post(url, data=order_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_list_users_orders_successful_200(self):
+        order_data = {
             'delivery': timezone.now() + timedelta(days=2),
             'quantity': 2,
-            'tasty': self.tasty.id,
-            'address': address.id
-    }
+            'tasty': self.tasty,
+            'address': self.client_user_address,
+            'owner': self.user
+        }
 
-    url = reverse('orders:order-list')
-    response = self.client.post(url, data=order_data)
-    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        baker.make(Order, **order_data)
+        url = reverse('orders:order-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = json.loads(response.content)
+        self.assertEqual(len(response), 1)
+
+
 
