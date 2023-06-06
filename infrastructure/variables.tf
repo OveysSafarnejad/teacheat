212ac691c6d8196cd11d8241f60307bf5121f372
Oveys Safarnejad <safarnejad@outlook.com>
2023-06-06 13:28:03 +0200

    Add rds module.

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,5 +4,21 @@ variable "base_region" {
 
 variable "prefix" {
   description = "A simple prefix for all resources related to teacheat application"
+  default     = "teacheat"
+}
+
+variable "postgres_db" {
   default = "teacheat"
+}
+
+variable "postgres_user" {
+  default = "dbuser"
+}
+
+variable "postgres_password" {
+  default = "s3cr3tpassword"
+}
+
+variable "postgres_port" {
+  default = "5450"
 }
